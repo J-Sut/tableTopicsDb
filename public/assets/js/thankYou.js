61c28ef83cf4e2569e272df33ed596323ac2a1bb
j-Sut <flowstategame@gmail.com>
2017-06-11 19:27:51 -0700

    created footer; display questions and intro side by side; all forms refined +more

--- public/assets/js/thankYou.js
+++ public/assets/js/thankYou.js
@@ -4,15 +4,35 @@
 
 /***************** f(Modify-state) *****************/
 
+// Count total number of submissions to the Db
+function countSubmissions(){
+
+		$.ajax({
+		type: "GET",
+		url: 'http://localhost:8080/topics',
+		success: function(data){
+			console.log(data.length);
+			displaySubmissionscount(data.length)
+		},
+		error: function(data){
+			console.log('Did not count submissions')
+		}
+	});	
+};
 
 
 /***************** f(Render-State) *****************/
 
+function displaySubmissionscount(count){
+	$('#currentTopicsCount').text(count);
+};
 
 
 /***************** Event Listeners *****************/
 
-
+$(function(){
+	countSubmissions();
+});
 
 
 
